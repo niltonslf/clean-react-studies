7d091888fb369817943067ca850d75332decd5e6
niltonslf <niltonrck@gmail.com>
2022-11-02 13:50:18 -0300

    refactor: change import to use module

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,13 +1,22 @@
-import { AuthenticationParams } from '../../../domain/usecases/authentication'
-import { HttpPostClient } from '../../protocols/http/http-post-client'
+import { faker } from '@faker-js/faker'
+import { AccountModel } from '@/domain/models/account-model'
+import {
+  Authentication,
+  AuthenticationParams
+} from '@/domain/usecases/authentication'
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     await this.httpPostClient.post({ url: this.url, body: params })
+
+    const accessToken = faker.random.alphaNumeric as any as string
+
+    return await Promise.resolve({ accessToken })
   }
 }
