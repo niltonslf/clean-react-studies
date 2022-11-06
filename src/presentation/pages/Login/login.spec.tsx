00870c06841eb943405882699181a38e2e281923
niltonslf <niltonrck@gmail.com>
2022-11-06 11:32:14 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,17 +1,33 @@
-import { render, RenderResult } from '@testing-library/react'
+import { Validation } from '@/presentation/protocols/validation'
+import { faker } from '@faker-js/faker'
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 
 import Login from './index'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
-  return { sut }
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
+  return { sut, validationSpy }
 }
 
 describe('Login Component', () => {
+  afterEach(cleanup)
+
   test('Should render with initial state', () => {
     const { sut } = makeSut()
     const { getByTestId } = sut
@@ -27,5 +43,15 @@ describe('Login Component', () => {
 
     const passwordInput = getByTestId('password') as HTMLInputElement
     expect(passwordInput.value).toBe('')
+  })
+
+  test('should call validation with correct value ', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailFaker = faker.internet.email()
+
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: emailFaker } })
+
+    expect(validationSpy.input).toEqual({ email: emailFaker })
   })
 })
