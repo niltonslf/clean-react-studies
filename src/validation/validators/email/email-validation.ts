daec2035d5ed42197c0e5447fd27f53d67cb0419
niltonslf <niltonrck@gmail.com>
2022-11-13 10:25:52 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -10,6 +10,6 @@ export class EmailValidation implements FieldValidation {
     const emailRegex =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
-    return emailRegex.test(value) ? null : new InvalidFieldError()
+    return !value || emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
