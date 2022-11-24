d9a70f5cee401cd19edb1d895a176d75f5edff83
niltonslf <niltonrck@gmail.com>
2022-11-24 14:57:10 -0300

    test: ensure Login present error if form is invalid

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -8,5 +10,15 @@ describe('Login', () => {
     cy.getByTestId('password-error').should('include.text', 'Campo inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('email-error').should('include.text', 'Campo inválido')
+
+    cy.getByTestId('password').type(faker.random.alphaNumeric(4))
+    cy.getByTestId('password-error').should('include.text', 'Campo inválido')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
   })
 })
