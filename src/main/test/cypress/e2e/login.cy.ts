8ef71b7386dfbc4a01a0a267da66fd5e322ef0bb
niltonslf <niltonrck@gmail.com>
2022-11-24 14:45:36 -0300

    feat: ensure login begins with correct initial state

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -1,5 +1,12 @@
 describe('Login', () => {
-  it('should render Loding', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('should render Login', () => {
+    cy.getByTestId('email-error').should('include.text', 'Campo obrigatório')
+    cy.getByTestId('password-error').should('include.text', 'Campo inválido')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
