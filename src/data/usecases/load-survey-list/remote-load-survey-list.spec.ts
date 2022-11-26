8468df71da1ad5dc8507a3785b11c565a7f68d43
niltonslf <niltonrck@gmail.com>
2022-11-26 15:01:50 -0300

    test: ensure RemoteLoadSurveyList returns a list of SurveyModel on 200

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -4,6 +4,7 @@ import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpGetClientSpy } from '@/data/test'
 import { UnexpectedError } from '@/domain/errors'
 import { SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
 import { faker } from '@faker-js/faker'
 
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
@@ -62,5 +63,19 @@ describe('RemoteLoadSurveyList', () => {
 
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return a list of SurveyModels if HttpGetClient returns 200', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+
+    const httpResult = mockSurveyListModel()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    }
+
+    const survey = await sut.loadAll()
+    expect(survey).toEqual(httpResult)
   })
 })
