d8759b93f8504dcc08e86e3d807a585370693e4d
niltonslf <niltonrck@gmail.com>
2022-11-29 20:58:20 -0300

    feat: add getCurrentAccount to ApiContext

--- src/presentation/context/api/api-context.ts
+++ src/presentation/context/api/api-context.ts
@@ -4,6 +4,7 @@ import { AccountModel } from '@/domain/models'
 
 type ApiProps = {
   setCurrentAccount?: (account: AccountModel) => void
+  getCurrentAccount?: () => AccountModel
 }
 
 export const ApiContext = createContext<ApiProps>({})
