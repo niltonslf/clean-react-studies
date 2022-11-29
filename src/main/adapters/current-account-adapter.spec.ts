103d64126ccc94b9582ea896c7e57ae24b466aae
niltonslf <niltonrck@gmail.com>
2022-11-29 08:55:49 -0300

    test: ensure CurrentAccountAdapter throws error if invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test, vi } from 'vitest'
 
+import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
@@ -15,5 +16,14 @@ describe('CurrentAccountAdapter', () => {
     setCurrentAccountAdapter(account)
 
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('should call throw unexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter({
+        accessToken: '',
+        name: '',
+      })
+    }).toThrow(new UnexpectedError())
   })
 })
