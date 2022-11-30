96da9666d3a7b4686be00d3bf38192175b0a0215
niltonslf <niltonrck@gmail.com>
2022-11-29 21:49:12 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,5 +1,6 @@
-import { beforeEach, describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
 
+import { mockAccountModel } from '@/domain/test'
 import { faker } from '@faker-js/faker'
 
 import 'vitest-localstorage-mock'
@@ -8,20 +9,42 @@ import { LocalStorageAdapter } from './local-storage-adapter'
 
 const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 
+// @ts-expect-error
+delete global.localStorage
+
+global.localStorage = {
+  getItem: vi.fn(),
+  setItem: vi.fn(),
+  clear: vi.fn(),
+  removeItem: vi.fn(),
+  key: vi.fn(),
+  length: faker.datatype.number(),
+}
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear()
   })
 
-  test('should call LocalStorage with correct values', () => {
+  test('should call LocalStorage.setItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
-    const value = {
-      accessToken: faker.datatype.uuid(),
-      name: faker.name.fullName(),
-    }
+    const value = mockAccountModel()
 
     sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+
+  test('should call LocalStorage.getItem with correct value', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = mockAccountModel()
+
+    const getItemSpy = vi.spyOn(localStorage, 'getItem').mockReturnValue(JSON.stringify(value))
+
+    const account = sut.get(key)
+
+    expect(account).toEqual(value)
+    expect(getItemSpy).toHaveBeenCalledWith(key)
   })
 })
