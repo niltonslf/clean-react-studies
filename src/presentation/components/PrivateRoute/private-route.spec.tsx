7550b0921ef6b582915b666c0990758cf3a03214
niltonslf <niltonrck@gmail.com>
2022-11-30 21:37:04 -0300

    test: move sut create to a factory method

--- src/presentation/components/PrivateRoute/private-route.spec.tsx
+++ src/presentation/components/PrivateRoute/private-route.spec.tsx
@@ -1,4 +1,4 @@
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import { expect, describe, test } from 'vitest'
 
@@ -6,10 +6,19 @@ import { render } from '@testing-library/react'
 
 import { PrivateRoute } from './private-route'
 
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+
+  return { history }
+}
+
 describe('PrivateRoute', () => {
   test('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-
+    const { history } = makeSut()
     render(
       <Router navigator={history} location={history.location}>
         <PrivateRoute />
