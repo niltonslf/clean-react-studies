b593be953aa1497731a7a66a63af6783943607d3
niltonslf <niltonrck@gmail.com>
2022-12-03 19:49:45 -0300

    test: ensure AuthorizeHttpGetClientDecorator return the same as HttpGetClient

--- src/main/decorator/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorator/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -85,4 +85,12 @@ describe('AuthorizeHttpGetClientDecorator', () => {
       'x-access-token': getStorageSpy.value.accessToken,
     })
   })
+
+  test('should return the same result as  HttpGetClient', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+
+    const httpResponse = await sut.get(mockGetRequest())
+
+    expect(httpResponse).toEqual(httpGetClientSpy.response)
+  })
 })
