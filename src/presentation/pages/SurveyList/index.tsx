93a86c92c833a59a54fe8b7734884276f8975af2
niltonslf <niltonrck@gmail.com>
2022-12-03 11:20:02 -0300

    feat: ensure SurveyList render surveyItem on success

--- src/presentation/pages/SurveyList/index.tsx
+++ src/presentation/pages/SurveyList/index.tsx
@@ -4,6 +4,7 @@ import { SurveyModel } from '@/domain/models'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 import { Header } from '@/presentation/components'
 
+import SurveyItem from './components/SurveyItem'
 import SurveyItemLoader from './components/SurveyItemLoader'
 
 import './survey-list-styles.scss'
@@ -13,7 +14,7 @@ type SurveyListProps = {
 }
 
 const SurveyList: React.FC<SurveyListProps> = ({ loadSurveyList }) => {
-  const [, setItems] = useState<SurveyModel[]>([])
+  const [items, setItems] = useState<SurveyModel[]>([])
 
   useEffect(() => {
     loadSurveyList.loadAll().then((response) => {
@@ -26,9 +27,15 @@ const SurveyList: React.FC<SurveyListProps> = ({ loadSurveyList }) => {
       <Header />
 
       <section className='survey-content'>
-        <h1 className='survey-content__title'>Enquetes</h1>
+        <h1 className='survey-content__title' data-testid='title'>
+          Enquetes
+        </h1>
         <div className='survey-content__list' data-testid='survey-list'>
-          <SurveyItemLoader />
+          {items.length ? (
+            items.map((survey) => <SurveyItem survey={survey} key={survey.id} />)
+          ) : (
+            <SurveyItemLoader />
+          )}
         </div>
       </section>
     </main>
