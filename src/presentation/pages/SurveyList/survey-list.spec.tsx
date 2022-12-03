4365acd58fa10ec0daae91027365be7ceb0e181a
niltonslf <niltonrck@gmail.com>
2022-12-03 09:03:55 -0300

    test: ensure loadSurveyList has been called after load the component

--- src/presentation/pages/SurveyList/survey-list.spec.tsx
+++ src/presentation/pages/SurveyList/survey-list.spec.tsx
@@ -1,10 +1,30 @@
 import { describe, expect, test } from 'vitest'
 
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 import { SurveyList } from '@/presentation/pages'
 import { render, screen } from '@testing-library/react'
 
-const makeSut = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount: number = 0
+
+  async loadAll(): Promise<SurveyModel[] | null> {
+    this.callsCount++
+    return null
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return {
+    loadSurveyListSpy,
+  }
 }
 
 describe('SurveyList Component', () => {
@@ -12,5 +32,10 @@ describe('SurveyList Component', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('.survey-loader').length).toBe(4)
+  })
+
+  test('should call loadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
